877fd162a22af64f6427cd7007652760acece9a4
IrynaJuriivna <iryna.felyk@gmail.com>
2020-06-01 23:20:51 +0300

    init

---
+++ script/app.js
@@ -0,0 +1,12 @@
+
+$('.sl').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow:3,
+    centerMode:true,
+    variableWidth: true
+}
+);
---
+++ script/app2.js
@@ -0,0 +1,11 @@
+
+$('.sl2').slick({
+    autoplay: true,
+    Speed: 500,
+    slidesToShow: 1,
+  fade: true,
+  
+  centerMode:true,
+  cssEase: 'linear'
+}
+);
